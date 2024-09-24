433da7e7998c1efed0bf8dd359186e683b4f96c2
larsg <lars.gunnar@jobloop.no>
2024-09-24 17:08:42 +0200

    setup development environment

---
+++ flake.nix
@@ -0,0 +1,49 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs =
+    { self, nixpkgs }:
+    let
+      allSupportedSystems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+      forEachSupportedSystems = nixpkgs.lib.genAttrs allSupportedSystems;
+    in
+    {
+      devShells = forEachSupportedSystems (
+        system:
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+            config.allowUnfree = true;
+          };
+        in
+        {
+          default = pkgs.mkShell {
+            # Project dependencies
+            packages = [
+              # Infrastructure management
+              pkgs.terraform # Infrastructure as Code tool
+              pkgs.azure-cli # For communicating with Azure through the terminal
+
+              # C# and .NET Core
+              pkgs.dotnetCorePackages.sdk_8_0 # Set of tools for developing a C# .NET application
+              pkgs.omnisharp-roslyn # Langauge Sever for C# and .NET
+            ];
+
+            # Environment Variables
+            # Expose the exact path to the Dotnet binaries
+            DOTNET_ROOT = builtins.toString pkgs.dotnetCorePackages.sdk_8_0;
+            DOTNET_BIN = "${pkgs.dotnetCorePackages.sdk_8_0}/bin/dotnet";
+          };
+        }
+      );
+    };
+}
